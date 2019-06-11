0703007d005cee734f775e964a8c83aa2b498ab3
Chad Nelson <chad.nelson@temple.edu>
2019-06-11 10:00:03 -0400

    Cleanup to standards

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_passenger_package(host):
+    package = host.package('mod_passenger')
+    assert package.is_installed
