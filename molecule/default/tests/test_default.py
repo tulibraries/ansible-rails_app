917d0f88bd4e4d8341d72f50c79bdd331493aa3e
Chad Nelson <chad.nelson@temple.edu>
2019-06-11 12:04:52 -0400

    Fix tests to actually pass

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_passenger_package(host):
-    package = host.package('mod_passenger')
-    assert package.is_installed
+def test_httpd_is_running(host):
+    service = host.service('httpd')
+    assert service.is_running
+
+
+def test_httpd_application_conf_created(host):
+    f = host.file('/etc/httpd/conf.d/applications.conf')
+    assert f.exists
+
+
+def test_rails_app_in_place(host):
+    app_dir = host.file("/var/www/nothing")
+    assert app_dir.is_directory
